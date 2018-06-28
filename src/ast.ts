e21b5a8c284bc13413267cc49cdff8652e36f808
Wesley Lin <wesleylin625@gmail.com>
2018-06-27 21:29:32 -0400

    remove random commit merge

--- src/ast.ts
+++ src/ast.ts
@@ -34,19 +34,15 @@ export class MemExp extends ASTNode {
   }
 }
 
-<<<<<<< HEAD
 // There are 4 types of CallExp
 // 1) clock() no args
 // 2) clock('one') w/ args
 // 3) clock{ show() } w/ body 
 // 4) clock('weird') { really? } both args and body
-=======
->>>>>>> master
 export class CallExp extends ASTNode {
   constructor(
     yylloc:yylloc, 
     public callee:any, 
-<<<<<<< HEAD
     public args:Array<any> = [],
     public body?:Block
   ) {
@@ -60,12 +56,6 @@ export class AssignExp extends ASTNode {
   operator = '='
   constructor(yylloc:yylloc, public left:any, public right:any) {
     super('AssignExp', yylloc)
-=======
-    public args:Array<any>,
-    public body:Block
-  ) {
-    super('CallExp', yylloc)
->>>>>>> master
   }
 }
 
@@ -76,11 +66,7 @@ export class Stmt extends ASTNode {
 }
 
 export class Block extends ASTNode {
-<<<<<<< HEAD
   constructor(yylloc:yylloc, public body:Array<Stmt>) {
-=======
-  constructor(yylloc:yylloc, public args:Array<Stmt>) {
->>>>>>> master
     super('Block', yylloc)
   }
 }
